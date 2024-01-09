b50722bc2100233e31895b6d279c007c60135e5f
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2024-01-09 15:13:17 -0500

    Update AppDelegate.swift

--- Taurine/app/AppDelegate.swift
+++ Taurine/app/AppDelegate.swift
@@ -14,7 +14,7 @@ struct Main {
         if args.count >= 2 {
             if args[1] == "jailbreak" {
                 DispatchQueue.global(qos: .utility).async {
-                    print("Jailbreaking... TEST: 7")
+                    print("Jailbreaking... TEST: 8")
                     UIApplication.shared.isIdleTimerDisabled = true
                     jailbreak()
                 }
@@ -45,30 +45,27 @@ func jailbreak() {
     let restoreRootFs = false
     let generator = NonceManager.shared.currentValue
     print("1")
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-        print("2")
-        usleep(500 * 1000)
-        print("3")
-        var hasKernelRw = false
-        var any_proc = UInt64(0)
-        print("Selecting kfd [smith] for iOS 14.0 - 14.8.1")
-        LogStream.shared.pause()
-        let ret = do_kopen(0x800, 0x1, 0x2, 0x2)
-        LogStream.shared.resume()
-        print("4")
-        if ret != 0 {
-            print("Successfully exploited kernel!");
-            any_proc = our_proc_kAddr
-            hasKernelRw = true
-        }
-        print("5")
-        guard hasKernelRw else {
-            print("No kernel r/w!")
-            return
-        }
-        let electra = Electra(ui: viewController, any_proc: any_proc, enable_tweaks: enableTweaks, restore_rootfs: restoreRootFs, nonce: generator)
-        let err = electra.jailbreak()
+    usleep(500 * 1000)
+    print("2")
+    var hasKernelRw = false
+    var any_proc = UInt64(0)
+    print("Selecting kfd [smith] for iOS 14.0 - 14.8.1")
+    LogStream.shared.pause()
+    let ret = do_kopen(0x800, 0x1, 0x2, 0x2)
+    LogStream.shared.resume()
+    print("3")
+    if ret != 0 {
+        print("Successfully exploited kernel!");
+        any_proc = our_proc_kAddr
+        hasKernelRw = true
     }
+    print("4")
+    guard hasKernelRw else {
+        print("No kernel r/w!")
+        return
+    }
+    let electra = Electra(ui: viewController, any_proc: any_proc, enable_tweaks: enableTweaks, restore_rootfs: restoreRootFs, nonce: generator)
+    let err = electra.jailbreak()
 }
 
 var viewController: ViewController = ViewController()
