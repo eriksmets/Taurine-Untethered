fa03f589f07d953e2e48357c461f074874874e1b
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2024-01-09 00:26:18 -0500

    Update AppDelegate.swift

--- Taurine/app/AppDelegate.swift
+++ Taurine/app/AppDelegate.swift
@@ -43,36 +43,14 @@ func jailbreak() {
     usleep(500 * 1000)
     var hasKernelRw = false
     var any_proc = UInt64(0)
-    switch ExploitManager.shared.chosenExploit {
-    case .cicutaVirosa:
-        print("Selecting cicuta_virosa for iOS 14.0 - 14.3")
-        if cicuta_virosa() == 0 {
-            any_proc = our_proc_kAddr
-            hasKernelRw = true
-        }
-    case .kfdPhysPuppet:
-        print("Selecting kfd [physpuppet] for iOS 14.0 - 14.8.1")
-        LogStream.shared.pause()
-        let ret = do_kopen(0x800, 0x0, 0x2, 0x2)
-        LogStream.shared.resume()
-        if ret != 0 {
-            print("Successfully exploited kernel!");
-            any_proc = our_proc_kAddr
-            hasKernelRw = true
-        }
-    case .kfdSmith:
-        print("Selecting kfd [smith] for iOS 14.0 - 14.8.1")
-        LogStream.shared.pause()
-        let ret = do_kopen(0x800, 0x1, 0x2, 0x2)
-        LogStream.shared.resume()
-        if ret != 0 {
-            print("Successfully exploited kernel!");
-            any_proc = our_proc_kAddr
-            hasKernelRw = true
-        }
-    default:
-        print("Unable to get kernel r/w")
-        return
+    print("Selecting kfd [smith] for iOS 14.0 - 14.8.1")
+    LogStream.shared.pause()
+    let ret = do_kopen(0x800, 0x1, 0x2, 0x2)
+    LogStream.shared.resume()
+    if ret != 0 {
+        print("Successfully exploited kernel!");
+        any_proc = our_proc_kAddr
+        hasKernelRw = true
     }
     guard hasKernelRw else {
         return
